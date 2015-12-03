91440f15c29b6335f50ffe3ae8da2da671668965
Matthew General <mgeneral@synacor.com>
2015-12-03 00:44:08 -0500

    Day 3

---
+++ Day3/task1.php
@@ -0,0 +1,36 @@
+<?php
+$input = file_get_contents("input");
+$grid = [];
+$x = 0;
+$y = 0;
+$grid['0|0'] = 1;
+$houses = 1;
+for ($i=0; $i < strlen($input); $i++) {
+	$direction = $input[$i];
+	switch ($direction) {
+		case "^":
+			// north
+			$y++;
+		break;
+		case "v":
+			// south
+			$y--;
+		break;
+		case ">":
+			// east
+			$x++;
+		break;
+		case "<":
+			//west
+			$x--;
+		break;
+	}
+	echo "Delivering to {$y} , {$x}\n";
+	if (!isset($grid[$y . '|' . $x]))
+	{
+		$houses++;
+	}
+	@$grid[$y . '|' . $x] += 1;
+}
+//var_dump($grid);
+echo $houses;
---
+++ Day3/task2.php
@@ -0,0 +1,43 @@
+<?php
+$input = file_get_contents("input");
+$grid = [];
+$x = 0;
+$y = 0;
+$grid['0|0'] = 1;
+$santa = ['x' => 0, 'y' => 0];
+$robosanta = ['x' => 0, 'y' => 0];
+$houses = 1;
+$turn = 'santa';
+for ($i=0; $i < strlen($input); $i++) {
+	$x = ${$turn}['x'];
+	$y = ${$turn}['y'];
+	$direction = $input[$i];
+	switch ($direction) {
+		case "^":
+			// north
+			$y++;
+		break;
+		case "v":
+			// south
+			$y--;
+		break;
+		case ">":
+			// east
+			$x++;
+		break;
+		case "<":
+			//west
+			$x--;
+		break;
+	}
+	if (!isset($grid[$y . '|' . $x]))
+	{
+		$houses++;
+	}
+	@$grid[$y . '|' . $x] += 1;
+
+	${$turn}['x'] = $x;
+	${$turn}['y'] = $y;
+	$turn = $turn == 'santa' ? 'robosanta' : 'santa';
+}
+echo $houses;
