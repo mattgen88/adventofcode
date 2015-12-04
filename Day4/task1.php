dea06fb61e63d56d1469df3f0870a1b4db983f7c
Matthew General <mgeneral@synacor.com>
2015-12-04 12:19:51 -0500

    Day 4

---
+++ Day4/task1.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = 'ckczppom';
+
+function mine($input, $num)
+{
+	$md5 = md5($input . $num);
+	$head = substr($md5, 0, 5);
+	if ($head === '00000')
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
+
+$num = 1;
+while (!mine($input, $num))
+{
+	$num++;
+}
+//echo $num;
+echo "MD5 of " . $input . $num . ": " . md5($input . $num);
+
+?>
