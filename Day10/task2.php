7afbc34026e3098a1b5c3484f90106868c9fb430
Matthew General <mgeneral@synacor.com>
2015-12-10 11:55:12 -0500

    Day 10: One one one zero

---
+++ Day10/task2.php
@@ -0,0 +1,32 @@
+<?php
+$input = '1113222113';
+
+
+$last = $input;
+for ($i = 0; $i < 50; $i++) {
+    //consume the characters
+    $lastChar = '';
+    $newString = '';
+    $currentCharCount = 0;
+    for ($j = 0; $j < strlen($last); $j++) {
+      // get nextChar
+      $nextChar = $last[$j];
+      // if nextChar is not the first char and it is different from last char
+      if ($lastChar != '' && $lastChar != $nextChar) {
+        // append the current count of chars to the string followed by the last char
+        // reset the currentCharCount
+        $newString .= "{$currentCharCount}{$lastChar}";
+        $currentCharCount=1;
+      } else {
+        $currentCharCount++;
+      }
+      $lastChar = $nextChar;
+    }
+    $newString .= "{$currentCharCount}{$lastChar}";
+    echo $newString;
+    $last = $newString;
+    echo "\n";
+}
+
+echo "Answer: " . strlen($last);
+?>
