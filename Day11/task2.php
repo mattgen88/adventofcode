1bcee99a2dfe0298eba5634add7a73f41b11862b
Matthew General <mgeneral@synacor.com>
2015-12-11 00:51:16 -0500

    Day 11: Reading comprehension is important...

---
+++ Day11/task2.php
@@ -0,0 +1,31 @@
+<?php
+
+$input = "vzbxxzaa";
+
+while (!testPassword($input)) {
+  echo "$input didn't pass\n";
+  // increment
+  $input = increment_letter($input);
+}
+
+echo "$input\n";
+
+function increment_letter($input) {
+  echo $input . "\n";
+  if ($input[strlen($input)-1] === 'z') {
+    $new_input = increment_letter(substr($input, 0, strlen($input)-1)) . 'a';
+  }
+  else {
+    $length = strlen($input);
+    $partial = substr($input, 0, $length-1);
+    $new_input = $partial . chr(ord($input[strlen($input)-1])+1);
+  }
+  return $new_input;
+}
+function testPassword($password) {
+  $confusion_check = (preg_match('/(i|o|l)/', $password) === 0);
+  $repeat_letter_check = preg_match('/([a-z])\1.*([a-z])\2/', $password);
+  $series_check = preg_match('/(abc|bcd|cde|def|efg|fgh|ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)/',$password);
+  return $confusion_check && $repeat_letter_check && $series_check;
+}
+ ?>
