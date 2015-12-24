b937f058e9f6f76a53370f77afa72b35bb99230a
Matthew General <mgeneral@synacor.com>
2015-12-24 11:19:11 -0500

    Day 19

---
+++ Day19/task1.php
@@ -0,0 +1,71 @@
+<?php
+$productions = explode("\n", "Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg");
+$molecule = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF";
+// $productions = explode("\n","H => HO
+// H => OH
+// O => HH");
+// $molecule = "HOHOHO";
+$molecules=[];
+$replacements = 0;
+foreach ($productions as $production) {
+  preg_match("/(\w+) => (\w+)/", $production, $matches);
+  list(,$from, $to) = $matches;
+  $count = substr_count($molecule, $from);
+  echo "Found $count instances of $from in $molecule\n";
+  $loc = 0;
+  while ($count > 0) {
+    // find the $count instance of $from and replace with $to
+    $loc = strpos($molecule, $from, $loc);
+    echo "Found $from at location $loc\n";
+    $new_molecule = substr_replace($molecule, $to, $loc, strlen($from));
+    $molecules[$new_molecule] = TRUE;
+    $loc++;
+    $count--;
+    $replacements++;
+  }
+}
+echo "Found " . count($molecules) . " Distinct Molecules over $replacements iterations\n";
+//var_dump($molecules);
+ ?>
---
+++ Day19/task2.php
@@ -0,0 +1,67 @@
+<?php
+$productions = explode("\n", "Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg");
+$molecule = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF";
+// $productions = explode("\n","H => HO
+// H => OH
+// O => HH");
+// $molecule = "HOHOHO";
+$molecules=[];
+$replacements = 0;
+while ($molecule !== 'e') {
+  foreach ($productions as $production) {
+    preg_match("/(\w+) => (\w+)/", $production, $matches);
+    list(,$to, $from) = $matches;
+    $count = substr_count($molecule, $from);
+    if ($count === 0) {
+      continue;
+    }
+    echo "Found $count instances of $from in $molecule replaced with $to\n";
+    $molecule = preg_replace("/($from)/", $to, $molecule);
+    $replacements += $count;
+  }
+}
+echo "Did $replacements replacements\n";
+//var_dump($molecules);
+ ?>
