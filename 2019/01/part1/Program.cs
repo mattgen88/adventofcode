0816542072fad1669641948c99cae0babfb01e3d
Matthew General <matthew.general@gmail.com>
2019-12-03 20:39:12 -0500

    Day 1, Part 1, in... csharp?

---
+++ 2019/01/part1/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+using System.IO;
+using static System.Convert;
+using System.Collections.Generic;
+using System;
+using static System.Math;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader(@"./input.txt");
+            string line;
+            List<Decimal> masses = new List<Decimal>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                masses.Add(ToDecimal(line));
+            }
+
+            file.Close();
+            List<Decimal> fuel_costs = new List<Decimal>();
+            foreach (Decimal mass in masses)
+            {
+                Decimal fuel = Floor(mass / 3) - 2;
+                fuel_costs.Add(fuel);
+            }
+
+            Decimal fuel_cost = 0;
+            foreach (Decimal cost in fuel_costs)
+            {
+                fuel_cost = fuel_cost + cost;
+                WriteLine(fuel_cost.ToString());
+            }
+            WriteLine("Total Fuel Cost {0}", fuel_cost);
+        }
+    }
+}
